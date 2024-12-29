f72e87c6e4ebb28c54a204dde3a2643b2e2612df
fuadmn <fuadmohamednor@gmail.com>
2024-12-29 11:53:29 +0300

    Form Design

--- index.php
+++ index.php
@@ -36,12 +36,29 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
+
+
+       <form>
+
+       <div class="form-group m-2">
+        <input type="text" name="" id="studentId" class="form-control" placeholder="Enter student Id">
+        
+       </div>
+       <div class="form-group m-2">
+       <input type="text" name="" id="studentName" class="form-control" placeholder="Enter student Name">        
+       </div>
+       <div class="form-group m-2">
+       <input type="text" name="" id="studentClass" class="form-control" placeholder="Enter student Class">        
+       </div>
+
+       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
       </div>
+
+       </form>
+      </div>
+      
     </div>
   </div>
 </div>
