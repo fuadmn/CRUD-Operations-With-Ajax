7b79f27f63fc9aa7b5337edd2a073e7d75a8bdf8
fuadmn <fuadmohamednor@gmail.com>
2024-12-29 13:46:19 +0300

    Build Your First API

--- api.php
+++ api.php
@@ -7,4 +7,37 @@ header("Content-type: application/json);
 // function Delete
 // function Update
 
+function raadAll($conn){
+ 
+ $data = array();
+ $massage = array();
+ //read All  students in the database
+ $query = "SELECT * FORM student";
+
+ // excute the quert
+
+ $result $conn->query($query);
+ 
+ // success or error
+
+ if($reqult){
+
+        while($row = $result->fetch_assoc()){
+    
+           $data [] = $row;
+       }
+
+       $massage = array("status" => true, "data" =? $data);
+
+ }else{
+
+       $massage = array("status" => false, "data" => $conn->error);
+
+       
+     }
+
+     echo json_encode($massage);
+
+}
+
 ?>
