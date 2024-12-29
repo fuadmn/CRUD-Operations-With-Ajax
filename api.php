9a505501de507eb5153f703cd34ed73fb7a06cb7
fuadmn <fuadmohamednor@gmail.com>
2024-12-29 13:04:49 +0300

    What is API ?

---
+++ api.php
@@ -0,0 +1,10 @@
+<?php
+
+header("Content-type: application/json);
+
+// function readAll
+// function Insert
+// function Delete
+// function Update
+
+?>
---
+++ conn.php
@@ -0,0 +1,10 @@
+<? php
+
+
+$conn = new mysqli("localhost","root","","mydb");
+
+if(#conn->connect_error){
+   echo $conn->error;
+}
+
+?>
