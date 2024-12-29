363e4de8ab1006989d25ae29a1c54a4ad2063089
fuadmn <fuadmohamednor@gmail.com>
2024-12-29 11:27:39 +0300

    Pop up Modal

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="col-sn-8">
 
       <h1 class="fs-3 text-center">Users Info</h1>
-
+     <button class="btn btn-success 1-3" id="addNew">Add New Students</button>
       <table class="table" >
 
       <thead>
@@ -27,6 +27,24 @@
       </thead>
       </table>
     </div>
+
+    <div class="modal" tabindex="-1" id="studentModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Users Info </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
 </div>
 
--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-// . # tag
+$("#addNew").click(function(){
 
-$(".heading_1").html("This is the js documents");
-
-
-$("#heading_2").css("backgroundColor", "#765675");
-
-$("h3").css("fontSize", "50px");
-
-// Event in jquery
-
-$("button").click(function(){
-    $("body").css("backgroundColor", "#876576");
+$("#studentModal").modal("show");
 })
