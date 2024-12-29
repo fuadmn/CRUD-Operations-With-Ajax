6c170e7efdcd22a45c1bd1fe82106c03e0ed53aa
fuadmn <fuadmohamednor@gmail.com>
2024-12-29 10:50:33 +0300

    Jquery Events

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 <h2 id="heading_2">Hello My Wolrd</h2>
 <h3>Hello My Wolrd</h3>
 
+<button>Click hrere</button>
+
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
 <script src="main.js"></script>
--- main.js
+++ main.js
@@ -6,3 +6,9 @@ $(".heading_1").html("This is the js documents");
 $("#heading_2").css("backgroundColor", "#765675");
 
 $("h3").css("fontSize", "50px");
+
+// Event in jquery
+
+$("button").click(function(){
+    $("body").css("backgroundColor", "#876576");
+})
